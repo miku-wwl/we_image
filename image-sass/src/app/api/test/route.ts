c0c7b7e7c2935b3869b03d93dfd9b784bc3b8b01
miku-wwl <569144003@qq.com>
2024-11-04 22:15:35 +0800

    feat:zod + drizzle

--- image-sass/src/app/api/test/route.ts
+++ image-sass/src/app/api/test/route.ts
@@ -1,27 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
-
-import z from "zod";
-
-const inputSchema = z.object({
-    name: z.string().max(10).min(3),
-    email: z.string().email(),
-});
+import { updateUserSchema } from "@/server/db/validate-schema";
 
 export function GET(request: NextRequest) {
     const query = request.nextUrl.searchParams;
 
-    const name = query.get("name");
+    // const name = query.get("name");
     const email = query.get("email");
+    // const id = query.get("id");
 
-    const result = inputSchema.safeParse({
-        name,
+    const result = updateUserSchema.safeParse({
+        // name,
         email,
+        // id,
     });
 
     if (result.success) {
         return NextResponse.json(result.data);
     } else {
+        console.error(result.error);
         return NextResponse.json({ error: result.error.message });
     }
 }
