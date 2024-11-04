4882cec6db89271c13f505348c7c183ec074d9b2
miku-wwl <569144003@qq.com>
2024-11-04 20:40:35 +0800

    feat: DrizzleAdapter

--- image-sass/src/app/api/auth/[...nextauth]/route.ts
+++ image-sass/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
-import NextAuth from "next-auth";
+import { db } from "@/server/db/db";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import NextAuth, { AuthOptions } from "next-auth";
 import GitlabProvider from "next-auth/providers/gitlab";
-export const authOptions = {
+export const authOptions: AuthOptions = {
+    adapter: DrizzleAdapter(db),
     // Configure one or more authentication providers
     providers: [
         GitlabProvider({
