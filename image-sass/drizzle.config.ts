d38d43bf6da029215b841ccab6b7391e33d91913
miku-wwl <569144003@qq.com>
2024-11-05 21:53:49 +0800

    feat: postgres

--- image-sass/drizzle.config.ts
+++ image-sass/drizzle.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     host: "117.72.69.172",
     port: 5432,
     user: "postgres",
-    password: "123123",
+    password: "32110219miku#",
     database: "miku",
   },
   verbose: true,
