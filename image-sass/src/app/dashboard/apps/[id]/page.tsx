dfaca4311add6111692bbf85f9df7bd7338876e1
miku-wwl <569144003@qq.com>
2024-11-18 22:26:46 +0800

    feat: Open a non-existent page

--- image-sass/src/app/dashboard/apps/[id]/page.tsx
+++ image-sass/src/app/dashboard/apps/[id]/page.tsx
@@ -2,8 +2,8 @@
 
 import { Uppy } from "@uppy/core";
 import AWSS3 from "@uppy/aws-s3";
-import { useState } from "react";
-import { trpcPureClient } from "@/utils/api";
+import { ReactNode, useEffect, useState } from "react";
+import { trpcClientReact, trpcPureClient } from "@/utils/api";
 import { Button } from "@/components/ui/Button";
 import { UploadButton } from "@/components/feature/UploadButton";
 import { Dropzone } from "@/components/feature/Dropzone";
@@ -19,6 +19,17 @@ export default function AppPage({
 }: {
     params: { id: string };
 }) {
+    const { data: apps, isPending } = trpcClientReact.apps.listApps.useQuery(
+        void 0,
+        {
+            refetchOnReconnect: false,
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+        }
+    );
+
+    const currentApp = apps?.filter((app) => app.id === appId)[0];
+
     const [uppy] = useState(() => {
         const uppy = new Uppy();
         uppy.use(AWSS3, {
@@ -53,52 +64,79 @@ export default function AppPage({
         order: "desc",
     });
 
-    return (
-        <div className="mx-auto h-full">
-            <div className="container flex justify-between items-center h-[60px]">
-                <Button
-                    onClick={() => {
-                        setOrderBy((current) => ({
-                            ...current,
-                            order: current?.order === "asc" ? "desc" : "asc",
-                        }));
-                    }}
-                >
-                    Created At{" "}
-                    {orderBy.order === "desc" ? <MoveUp /> : <MoveDown />}
-                </Button>
-                <div className="flex justify-center gap-2">
-                    <UploadButton uppy={uppy}></UploadButton>
-                    <Button asChild>
-                        <Link href="./new">new app</Link>
-                    </Button>
-                    <Button asChild>
-                        <Link href={`/dashboard/apps/${appId}/setting/storage`}>
-                            <Settings></Settings>
-                        </Link>
-                    </Button>
+    let children: ReactNode;
+
+    if (isPending) {
+        children = <div>Loading...</div>;
+    } else if (!currentApp) {
+        children = (
+            <div className="flex flex-col mt-10 p-4 border rounded-md max-w-48 mx-auto items-center">
+                <p className="text-lg">App Not Exist</p>
+                <p className="text-sm">Chose another one</p>
+                <div className="flex flex-col gap-4 items-center">
+                    {apps?.map((app) => (
+                        <Button key={app.id} asChild variant="link">
+                            <Link href={`/dashboard/apps/${app.id}`}>
+                                {app.name}
+                            </Link>
+                        </Button>
+                    ))}
                 </div>
             </div>
+        );
+    } else {
+        children = (
+            <div className="mx-auto h-full">
+                <div className="container flex justify-between items-center h-[60px]">
+                    <Button
+                        onClick={() => {
+                            setOrderBy((current) => ({
+                                ...current,
+                                order:
+                                    current?.order === "asc" ? "desc" : "asc",
+                            }));
+                        }}
+                    >
+                        Created At{" "}
+                        {orderBy.order === "desc" ? <MoveUp /> : <MoveDown />}
+                    </Button>
+                    <div className="flex justify-center gap-2">
+                        <UploadButton uppy={uppy}></UploadButton>
+                        <Button asChild>
+                            <Link href="./new">new app</Link>
+                        </Button>
+                        <Button asChild>
+                            <Link
+                                href={`/dashboard/apps/${appId}/setting/storage`}
+                            >
+                                <Settings></Settings>
+                            </Link>
+                        </Button>
+                    </div>
+                </div>
 
-            <Dropzone uppy={uppy} className=" relative h-[calc(100%-60px)]">
-                {(draging) => {
-                    return (
-                        <>
-                            {draging && (
-                                <div className=" absolute inset-0 bg-secondary/50 z-10 flex justify-center items-center text-3xl">
-                                    Drop File Here to Upload
-                                </div>
-                            )}
-                            <FileList
-                                appId={appId}
-                                uppy={uppy}
-                                orderBy={orderBy}
-                            ></FileList>
-                        </>
-                    );
-                }}
-            </Dropzone>
-            <UploadPreview uppy={uppy}></UploadPreview>
-        </div>
-    );
+                <Dropzone uppy={uppy} className=" relative h-[calc(100%-60px)]">
+                    {(draging) => {
+                        return (
+                            <>
+                                {draging && (
+                                    <div className=" absolute inset-0 bg-secondary/50 z-10 flex justify-center items-center text-3xl">
+                                        Drop File Here to Upload
+                                    </div>
+                                )}
+                                <FileList
+                                    appId={appId}
+                                    uppy={uppy}
+                                    orderBy={orderBy}
+                                ></FileList>
+                            </>
+                        );
+                    }}
+                </Dropzone>
+                <UploadPreview uppy={uppy}></UploadPreview>
+            </div>
+        );
+    }
+
+    return children;
 }
