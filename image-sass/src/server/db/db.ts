cfa707ce1772f676db96fef76c424b5f1dafaa1c
miku-wwl <569144003@qq.com>
2025-05-28 01:45:58 +0800

    feat: remove sensitive information

--- image-sass/src/server/db/db.ts
+++ image-sass/src/server/db/db.ts
@@ -5,6 +5,6 @@ import * as schema from "./schema";
 
 // for query purposes
 const queryClient = postgres(
-    "postgres://postgres:32110219miku123@117.72.69.172:5432/miku"
+    "postgres://postgres:@127.0.0.1:5432/"
 );
 export const db = drizzle(queryClient, { schema, logger: true });
